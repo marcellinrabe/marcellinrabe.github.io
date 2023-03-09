02dfb57a224f315c1ef0007f9cb99d711c660218
marcellinrabe <marcellinp20.aps1a@gmail.com>
2023-03-09 08:32:15 +0300

    ui - view project item

--- web/src/sections/Project.jsx
+++ web/src/sections/Project.jsx
@@ -11,45 +11,50 @@ export default function Project({ publicRepos }) {
                         Réalisations
                     </h1>
                 </div>
+                <div className="mt-3">
+                    <span className="fst-italic">data from github</span>
+                </div>
                 <ResponsiveMasonry columnsCountBreakPoints={{ 320: 2, 992: 3 }}>
                     <Masonry className="gap-3">
                         {publicRepos.map((repo) => (
-                            <div
-                                data-aos="zoom-in-up"
-                                data-aos-once="true"
-                                key={repo.id}
-                                className="w-100 rounded border border-2 my-2 p-1 bg-white position-relative"
-                            >
-                                <div className="w-100">
-                                    <img
-                                        src={repo.img_preview}
-                                        alt="img"
-                                        className="w-100 h-100 rounded"
-                                    />
-                                </div>
-                                <div className="position-absolute rounded top-0 start-0 px-2 overlay hovered h-100 text-light">
-                                    <div className="d-flex align-items-center h-100">
-                                        <div>
+                            <div className="w-100">
+                                <h3 className="font-title">{repo.name}</h3>
+                                <div
+                                    data-aos="zoom-in-up"
+                                    data-aos-once="true"
+                                    key={repo.id}
+                                    className="w-100 rounded border border-2 my-2 p-1 bg-white position-relative"
+                                >
+                                    <div className="w-100">
+                                        <img
+                                            src={repo.img_preview}
+                                            alt="img"
+                                            className="w-100 h-100 rounded"
+                                        />
+                                    </div>
+                                    <div className="w-100 position-absolute rounded top-0 start-0 px-2 overlay hovered h-100 text-light">
+                                        <div className="d-flex align-items-center justify-content-center w-100 h-100">
                                             <div>
-                                                <h3 className="fw-bold font-title text-center fs-5">
-                                                    {repo.name}
-                                                </h3>
+                                                <div className="flex-grow-1">
+                                                    <p>{repo.desc}</p>
+                                                </div>
+                                                <ul className="d-flex p-0 justify-content-around gap-2">
+                                                    <li>
+                                                        <a href={repo.homepage}>
+                                                            <SlScreenDesktop />
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a
+                                                            href={
+                                                                repo.github_url
+                                                            }
+                                                        >
+                                                            <FaGithub />
+                                                        </a>
+                                                    </li>
+                                                </ul>
                                             </div>
-                                            <div className="flex-grow-1">
-                                                <p>{repo.desc}</p>
-                                            </div>
-                                            <ul className="d-flex p-0 justify-content-around gap-2">
-                                                <li>
-                                                    <a href={repo.homepage}>
-                                                        <SlScreenDesktop />
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a href={repo.github_url}>
-                                                        <FaGithub />
-                                                    </a>
-                                                </li>
-                                            </ul>
                                         </div>
                                     </div>
                                 </div>
