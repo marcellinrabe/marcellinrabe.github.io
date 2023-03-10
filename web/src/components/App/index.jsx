b574ba3464f6f371d463d87eb0d734be2dcaa385
marcellinrabe <marcellinp20.aps1a@gmail.com>
2023-03-11 00:04:27 +0300

    perf - left react-router all in one loader in App component to component-based data fetching using react-query, for have beautiful skeleton until data is fetched :relieved:

--- web/src/components/App/index.jsx
+++ web/src/components/App/index.jsx
@@ -18,111 +18,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 import axios from 'axios';
 import Footer from '../Footer';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
-export async function loader() {
-    // Define the URL of your server's API endpoint
-    const API_URL = 'https://marcellinrabe-portfolio-server.onrender.com';
+const queryClient = new QueryClient();
 
-    const [userFromServer, publicRepos] = await Promise.all([
-        axios.get(`${API_URL}/user/me`).then((res) => res.data),
-        axios.get(`${API_URL}/public-repos`).then((res) => res.data),
-    ]);
-
-    return defer({
-        user: userFromServer,
-        publicRepos: publicRepos,
-    });
-}
 /**
  *
  * @components IconThemeProvider - apply custom config to icon from "react-icons" library
  * @returns jsx
  */
-function App() {
-    const datas = useLoaderData();
 
+const AppRouteProvider = () => (
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+);
+
+export { AppRouteProvider };
+
+function App() {
     return (
         <div className="container">
             <IconThemeProvider>
                 <NavBar />
-                <Suspense
-                    fallback={
-                        <ContentLoader viewBox="0 0 380 70">
-                            <rect
-                                x="0"
-                                y="0"
-                                rx="5"
-                                ry="5"
-                                width="70"
-                                height="70"
-                            />
-                            <rect
-                                x="80"
-                                y="17"
-                                rx="4"
-                                ry="4"
-                                width="300"
-                                height="13"
-                            />
-                            <rect
-                                x="80"
-                                y="40"
-                                rx="3"
-                                ry="3"
-                                width="250"
-                                height="10"
-                            />
-                        </ContentLoader>
-                    }
-                >
-                    <Await
-                        resolve={datas.user}
-                        errorElement={<div>Error Loading</div>}
-                    >
-                        {(user) => {
-                            return <Profile user={user} />;
-                        }}
-                    </Await>
-                </Suspense>
+                <Profile />;
                 <Skills />
-                <Suspense
-                    fallback={
-                        <ContentLoader viewBox="0 0 380 70">
-                            <rect
-                                x="0"
-                                y="0"
-                                rx="5"
-                                ry="5"
-                                width="70"
-                                height="70"
-                            />
-                            <rect
-                                x="80"
-                                y="17"
-                                rx="4"
-                                ry="4"
-                                width="300"
-                                height="13"
-                            />
-                            <rect
-                                x="80"
-                                y="40"
-                                rx="3"
-                                ry="3"
-                                width="250"
-                                height="10"
-                            />
-                        </ContentLoader>
-                    }
-                >
-                    <Await
-                        resolve={datas.publicRepos}
-                        errorElement={<div>Error Loading</div>}
-                    >
-                        {(publicRepos) => <Project publicRepos={publicRepos} />}
-                    </Await>
-                </Suspense>
-
+                <Project />
                 <Contact />
                 <Footer />
             </IconThemeProvider>
