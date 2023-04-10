2fcb1fb45852b37e3112eed635c79d29733288a1
marcellinrabe <marcellinp20.aps1a@gmail.com>
2023-04-10 12:08:28 +0300

    FIX - add cors options for the allowed origin only

--- server/src/app.js
+++ server/src/app.js
@@ -108,6 +108,18 @@ const originMiddleware = (req, res, next) => {
     }
 };
 
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
+
 // Utilisez le middleware CORS configuré pour toutes les routes
 app.use(originMiddleware);
 
